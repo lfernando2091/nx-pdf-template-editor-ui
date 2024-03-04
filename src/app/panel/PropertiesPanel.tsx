eec2becd6c113a54346cf47abf4c8e3c93bd76e5
Luis Hernandez <lfernando2091@gmail.com>
2024-03-03 20:34:12 -0600

    working

---
+++ src/app/panel/PropertiesPanel.tsx
@@ -0,0 +1,12 @@
+import { 
+    Text
+} from '@radix-ui/themes';
+
+export const PropertiesPanel = () => {
+
+    return (
+        <>
+        <Text size="2">Props.</Text>
+        </>
+    )
+}
--- src/app/section/DrawerContent.tsx
+++ src/app/section/DrawerContent.tsx
@@ -3,25 +3,26 @@ import {
     Box, 
     Tabs, 
 } from '@radix-ui/themes';
+import { PropertiesPanel } from '../panel/PropertiesPanel';
 
 export const DrawerContent = () => {
     return (
         <>
         <Tabs.Root defaultValue="props">
-                <Tabs.List>
-                  <Tabs.Trigger value="props">Properties</Tabs.Trigger>
-                  <Tabs.Trigger value="layers">Layers</Tabs.Trigger>
-                </Tabs.List>
+          <Tabs.List>
+            <Tabs.Trigger value="props">Properties</Tabs.Trigger>
+            <Tabs.Trigger value="layers">Layers</Tabs.Trigger>
+          </Tabs.List>
 
-                <Box px="4" pt="3" pb="2">
-                  <Tabs.Content value="props">
-                    <Text size="2">Props.</Text>
-                  </Tabs.Content>
-                  <Tabs.Content value="layers">
-                    <Text size="2">Layers.</Text>
-                  </Tabs.Content>
-                </Box>
-            </Tabs.Root>
+          <Box px="4" pt="3" pb="2">
+            <Tabs.Content value="props">
+              <PropertiesPanel/>
+            </Tabs.Content>
+            <Tabs.Content value="layers">
+              <Text size="2">Layers.</Text>
+            </Tabs.Content>
+          </Box>
+        </Tabs.Root>
         </>
     )
 }
