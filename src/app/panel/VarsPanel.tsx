1a2800ccf52e49f98d67a631ee53177133d34da2
Luis Hernandez <lfernando2091@gmail.com>
2024-03-06 11:03:59 -0600

    working

--- src/app/panel/VarsPanel.tsx
+++ src/app/panel/VarsPanel.tsx
@@ -1,5 +1,5 @@
-import { PinTopIcon } from "@radix-ui/react-icons"
-import { Box, Flex, Grid, TextField } from "@radix-ui/themes"
+import { Box, Grid, TextArea, TextField, Text, Button } from "@radix-ui/themes"
+import { useAppContext } from "../states/AppContext"
 
 type VarsPanelProps = {
 
@@ -8,21 +8,57 @@ type VarsPanelProps = {
 export const VarsPanel = ({
 
 }: VarsPanelProps) => {
+    const { template } = useAppContext()
+    const variables = template.variables ?? {}
     return (
         <>
         <Grid columns="2" gap="1" width="max-content">
-            <Box width="9">
-                <TextField.Root mb="1" size="1">
-                    <TextField.Slot><PinTopIcon/></TextField.Slot>
-                    <TextField.Input/>
-                </TextField.Root>
-            </Box>
-            <Box>
-                <TextField.Root mb="1" size="1">
-                    <TextField.Slot><PinTopIcon/></TextField.Slot>
-                    <TextField.Input/>
-                </TextField.Root>
-            </Box>
+            {Object.keys(variables).map((e) => (<>
+                <Box>
+                    <TextField.Root
+                    variant="soft" 
+                    mb="1" 
+                    size="1">
+                        <TextField.Input 
+                        value={e}
+                        placeholder="Variable name"/>
+                    </TextField.Root>
+                </Box>
+                <Box>
+                    <TextArea 
+                        mb="1" size="1"
+                        variant="soft"
+                        value={variables[e].value}
+                        placeholder="Variable content"
+                        rows={3} />
+                </Box>
+            </>))}
+        </Grid>
+        <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+            <Button variant="soft" style={{ width: "100%" }}>
+                Add new
+            </Button>
+        </div>
+
+        <Text size="2" mb="3" mt="2" weight="bold">
+            Default variables
+        </Text>
+        <Grid columns="2" mt="2" gap="1" width="max-content">
+                <Box>
+                    <TextField.Root
+                    variant="soft" 
+                    mb="1" 
+                    size="1">
+                        <TextField.Input 
+                        defaultValue="PAGE_NUM"
+                        readOnly/>
+                    </TextField.Root>
+                </Box>
+                <Box>
+                    <Text size="1" mb="2" mt="2" weight="light">
+                       Gets the current page number.
+                    </Text>
+                </Box>
         </Grid>
         </>
     )
